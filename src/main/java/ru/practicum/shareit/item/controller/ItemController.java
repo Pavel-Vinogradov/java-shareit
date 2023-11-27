e3e9970f6c4e37eadecfb7ce31ba35156cc51178
Pavel Vinogradov <pavelvinogradov45@gmail.com>
2023-11-27 23:59:07 +0300

    sprint  14

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -3,7 +3,9 @@ package ru.practicum.shareit.item.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.comment.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.service.ItemService;
 
 import javax.validation.Valid;
@@ -11,6 +13,9 @@ import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static ru.practicum.shareit.item.mapper.ItemMapper.toItemDto;
 
 /**
  * TODO Sprint add-controllers.
@@ -27,21 +32,22 @@ public class ItemController {
     public ItemDto saveItem(@RequestBody @Valid ItemDto itemDto,
                             @RequestHeader(name = USER_ID_HEADER) long userId) {
         log.info("Получен POST-запрос /items {} ", itemDto);
-        return itemService.saveItem(itemDto, userId);
+        return toItemDto(itemService.saveItem(itemDto, userId));
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto updateItem(@RequestHeader(name = USER_ID_HEADER) Long userId,
+    public ItemDto updateItem(@RequestHeader(name = USER_ID_HEADER) long userId,
                               @RequestBody ItemDto itemDto, @PathVariable Long itemId) {
         log.info("Получен PATCH-запрос /itemId {} ", itemId);
         itemDto.setId(itemId);
-        return itemService.updateItem(itemDto, userId);
+        return toItemDto(itemService.updateItem(itemDto, userId));
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto getItemById(@PathVariable Long itemId) {
+    public ItemDto getItemById(@RequestHeader(name = USER_ID_HEADER) long userId,
+                               @PathVariable Long itemId) {
         log.info("Получен GET-запрос /itemId {} ", itemId);
-        return itemService.getItemById(itemId);
+        return itemService.getItemById(userId, itemId);
     }
 
     @GetMapping
@@ -56,14 +62,23 @@ public class ItemController {
             return new ArrayList<>();
         }
         log.info("Получен GET-запрос /text {} ", text);
-        return itemService.searchItem(text);
+        return itemService.searchItem(text).stream()
+                .map(ItemMapper::toItemDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public CommentDto saveComment(@RequestHeader(name = USER_ID_HEADER) long userId,
+                                  @PathVariable Long itemId,
+                                  @RequestBody CommentDto commentDto) {
+        log.info("Получен POST-запрос: добавление отзыва о бронировании предмета {} ", itemId);
+        return itemService.postComment(userId, itemId, commentDto);
     }
 
     @DeleteMapping("/{itemId}")
     public void deleteItem(@RequestHeader(name = USER_ID_HEADER) long userId,
                            @PathVariable long itemId) {
-        log.info("Получен DELETE- запрос на удаление вещи {} ", itemId);
+        log.info("Получен DELETE- запрос на уаление предмета {} ", itemId);
         itemService.deleteItemById(userId, itemId);
     }
-
 }
